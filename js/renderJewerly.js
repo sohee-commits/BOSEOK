80d35e7acdd93f9b9d450141ffb6b2fe9e0be12c
소희 <105264974+sohee-commits@users.noreply.github.com>
2024-03-20 23:51:12 +0500

    added main database
    database; render; pages

--- js/renderJewerly.js
+++ js/renderJewerly.js
@@ -1,51 +1,139 @@
 const jewerly = [
 	// earrings
-	{ name: 'Lunette', price: '1,290', metal: 'silver', type: 'earrings' },
-	{ name: 'Versailles', price: '2,690', metal: 'silver', type: 'earrings' },
-	{ name: 'Marseille', price: '1,390', metal: 'silver', type: 'earrings' },
-	{ name: 'Flair', price: '990', metal: 'silver', type: 'earrings' },
-	{ name: 'Sienna', price: '1,750', metal: 'silver', type: 'earrings' },
-	{ name: 'Yvette', price: '1,690', metal: 'silver', type: 'earrings' },
-	{ name: 'Ballad', price: '990', metal: 'silver', type: 'earrings' },
-	{ name: 'Eternity', price: '1,190', metal: 'silver', type: 'earrings' },
+	{
+		name: 'Lunette',
+		price: '1,290',
+		type: 'earrings',
+		collection: `yeonhwa`,
+	},
+	{
+		name: 'Versailles',
+		price: '2,690',
+		type: 'earrings',
+		collection: ``,
+	},
+	{
+		name: 'Marseille',
+		price: '1,390',
+		type: 'earrings',
+		collection: `yeonhwa`,
+	},
+	{
+		name: 'Flair',
+		price: '990',
+		type: 'earrings',
+		collection: ``,
+	},
+	{
+		name: 'Sienna',
+		price: '1,750',
+		type: 'earrings',
+		collection: ``,
+	},
+	{
+		name: 'Yvette',
+		price: '1,690',
+		type: 'earrings',
+		collection: `yeonhwa`,
+	},
+	{
+		name: 'Ballad',
+		price: '990',
+		type: 'earrings',
+		collection: ``,
+	},
+	{
+		name: 'Eternity',
+		price: '1,190',
+		type: 'earrings',
+		collection: `yeonhwa`,
+	},
 	// rings
 	{
-		name: 'Petite Elodie Solitaire',
+		name: 'Petite Elodie',
 		price: '1,190',
-		metal: 'silver',
 		type: 'rings',
+		collection: ``,
 	},
-	{ name: 'Demi', price: '1,190', metal: 'silver', type: 'rings' },
-	{ name: 'Petite', price: '1,190', metal: 'silver', type: 'rings' },
+	{
+		name: 'Demi',
+		price: '1,190',
+		type: 'rings',
+		collection: ``,
+	},
+	{
+		name: 'Petite',
+		price: '1,190',
+		type: 'rings',
+		collection: ``,
+	},
 	{
 		name: 'Petite Comfort Fit Solitaire',
 		price: '1,190',
-		metal: 'silver',
 		type: 'rings',
+		collection: ``,
 	},
 	{
 		name: 'Camellia Milgrain',
 		price: '1,190',
-		metal: 'silver',
 		type: 'rings',
+		collection: ``,
 	},
-	{ name: 'Nadia', price: '1,190', metal: 'silver', type: 'rings' },
-	{ name: 'Luxe Viviana', price: '1,190', metal: 'silver', type: 'rings' },
-	{ name: 'Aria Three', price: '1,190', metal: 'silver', type: 'rings' },
-	// yeonhwa
-	// hwayang
+	{
+		name: 'Nadia',
+		price: '1,190',
+		type: 'rings',
+		collection: ``,
+	},
+	{
+		name: 'Luxe Viviana',
+		price: '1,190',
+		type: 'rings',
+		collection: ``,
+	},
+	{
+		name: 'Aria Three',
+		price: '1,190',
+		type: 'rings',
+		collection: ``,
+	},
+	// hwayang collection
+	{
+		name: 'Yaretzi',
+		price: '1,350',
+		type: 'necklace',
+		collection: `hwayang`,
+	},
+	{
+		name: 'Nadira',
+		price: '1,790',
+		type: 'necklace',
+		collection: `hwayang`,
+	},
+	{
+		name: 'Orinthia',
+		price: '750',
+		type: 'necklace',
+		collection: `hwayang`,
+	},
+	{
+		name: 'Alveena',
+		price: '1,050',
+		type: 'necklace',
+		collection: `hwayang`,
+	},
 ];
 
 let cards = document.querySelector(`.cards`);
 
 // all the jewerly
 let renderJewerly = () => {
-	for (let i = 0; i <= earrings.length - 1; i++) {
+	for (let i = 0; i <= jewerly.length - 1; i++) {
 		cards.innerHTML += `
     <!-- card -->
-    <a href="" class="card">
+    <a href="./${jewerly[i].name}.html" class="card">
       <div class="image">
-        <img src="./assets/jewerly/${i}.png" alt="preview" />
+        <img src="./assets/jewerly/${jewerly[i].name}.jpg" alt="preview" />
       </div>
       <p class="bold">${jewerly[i].name}</p>
       <p>$ ${jewerly[i].price}</p>
@@ -61,9 +149,9 @@ let renderEarrings = () => {
 	for (let i = 0; i <= earrings.length - 1; i++) {
 		cards.innerHTML += `
     <!-- card -->
-    <a href="" class="card">
+    <a href="./${earrings[i].name}.html" class="card">
       <div class="image">
-        <img src="./assets/earrings/${i}.png" alt="preview" />
+        <img src="./assets/jewerly/${earrings[i].name}.jpg" alt="preview" />
       </div>
       <p class="bold">${earrings[i].name}</p>
       <p>$ ${earrings[i].price}</p>
@@ -79,12 +167,48 @@ let renderRings = () => {
 	for (let i = 0; i <= rings.length - 1; i++) {
 		cards.innerHTML += `
     <!-- card -->
-    <a href="" class="card">
+    <a href="./${rings[i].name}.html" class="card">
       <div class="image">
-        <img src="./assets/rings/${i}.png" alt="preview" />
+        <img src="./assets/jewerly/${rings[i].name}.jpg" alt="preview" />
       </div>
       <p class="bold">${rings[i].name}</p>
       <p>$ ${rings[i].price}</p>
+    </a>
+    `;
+	}
+};
+
+// hwayang
+let hwayang = jewerly.filter((item) => item.collection === 'hwayang');
+
+let renderHwayang = () => {
+	for (let i = 0; i <= hwayang.length - 1; i++) {
+		cards.innerHTML += `
+    <!-- card -->
+    <a href="./${hwayang[i].name}.html" class="card">
+      <div class="image">
+        <img src="./assets/jewerly/${hwayang[i].name}.jpg" alt="preview" />
+      </div>
+      <p class="bold">${hwayang[i].name}</p>
+      <p>$ ${hwayang[i].price}</p>
+    </a>
+    `;
+	}
+};
+
+// yeonhwa
+let yeonhwa = jewerly.filter((item) => item.collection === 'yeonhwa');
+
+let renderYeonhwa = () => {
+	for (let i = 0; i <= yeonhwa.length - 1; i++) {
+		cards.innerHTML += `
+    <!-- card -->
+    <a href="./${yeonhwa[i].name}.html" class="card">
+      <div class="image">
+        <img src="./assets/jewerly/${yeonhwa[i].name}.jpg" alt="preview" />
+      </div>
+      <p class="bold">${yeonhwa[i].name}</p>
+      <p>$ ${yeonhwa[i].price}</p>
     </a>
     `;
 	}
