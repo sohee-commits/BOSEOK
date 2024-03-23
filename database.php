ab366020b70baccda53eb3758eccd163bc04fc78
소희 <mintopiate@gmail.com>
2024-03-23 09:08:50 +0500

    Update database.php

--- database.php
+++ database.php
@@ -1,8 +1,8 @@
 <?php
 $hostName = "localhost";
-$dbUser = "root";
-$dbPassword = "";
-$dbName = "login";
+$dbUser = "1is-a12_1is-a12";
+$dbPassword = "DN1J1JSHLe";
+$dbName = "1is-a12_1is-a12"; // НАЗВАТЬ SQL ФАЙЛ ТАК ЖЕ
 
 $mysqli = mysqli_connect($hostName, $dbUser, $dbPassword, $dbName);
 
