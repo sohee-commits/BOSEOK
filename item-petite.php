eb2affe5b0a882e09c975894c72a104083ac530d
소희 <105264974+sohee-commits@users.noreply.github.com>
2024-03-25 09:03:07 +0500

    user session check

--- item-petite.php
+++ item-petite.php
@@ -1,8 +1,5 @@
 <?php
-session_start();
-if (isset ($_SESSION["user"])) {
-	header("Location: index.php");
-}
+require_once "session.php";
 ?>
 <!DOCTYPE html>
 <html lang="en">
--- jewerly.php
+++ jewerly.php
@@ -1,3 +1,6 @@
+<?php
+require_once "session.php";
+?>
 <!DOCTYPE html>
 <html lang="en">
 	<head>
